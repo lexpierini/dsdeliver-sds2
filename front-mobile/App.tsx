6b98caf67c54b71ba72303904c3c678dadbd7bcf
lexpierini <alex_pierini@hotmail.com>
2021-01-10 02:03:37 -0500

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   useFonts,
   OpenSans_400Regular,
@@ -5,10 +6,8 @@ import {
 } from "@expo-google-fonts/open-sans";
 import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Header from "./src/Header";
-import Home from "./src/Home";
+import Routes from "./src/Routes";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -23,8 +22,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home />
+      <Routes />
     </View>
   );
 }
